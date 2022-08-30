4bfa40f6275dc9e289e54eb81a0838769195e395
inull <10429190+innerNULL@users.noreply.github.com>
2022-08-30 13:49:44 +0800

    Time: 3 ms (37.54%), Space: 6 MB (22.77%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,38 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int64_t first_one_idx = -1;
+        
+        int64_t start_idx = 0;
+        int64_t end_idx = n - 1;
+        int64_t mid_idx = -1;
+        
+        int64_t mid_val = -1;
+        while (true) {
+            //printf("info: start_idx=%i, end_idx=%i\n", start_idx, end_idx);
+            mid_idx = (start_idx + end_idx) / 2;
+            mid_val = mid_idx + 1;
+            //printf("info: mid_idx=%i, mid_val=%i\n", mid_idx, mid_val);
+            if (isBadVersion(mid_val)) {
+                first_one_idx = mid_idx;
+                end_idx = mid_idx;
+            } else {
+                start_idx = mid_idx;
+            }
+            //printf("info: start_idx=%i, end_idx=%i\n", start_idx, end_idx);
+            if (start_idx == end_idx) { break; }
+            if (start_idx == end_idx - 1) {
+                int32_t end_val = end_idx + 1;
+                if (isBadVersion(end_val)) {
+                    first_one_idx = end_idx;
+                    end_idx -= 1;
+                }
+            }            
+        }
+        int64_t first_one_val = first_one_idx + 1;
+        return first_one_val;
+    }
+};
