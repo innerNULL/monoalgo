7c3fd648a3c1ef0d3241416018e5f1274601206e
inull <10429190+innerNULL@users.noreply.github.com>
2022-09-05 11:26:11 +0800

    Time: 3 ms (37.44%), Space: 5.8 MB (88.94%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -1,9 +1,33 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
+
 class Solution {
 public:
     int firstBadVersion(int n) {
+        int64_t first_bad_idx = -1;
+        
+        int64_t start_idx = 0;
+        int64_t end_idx = n - 1;
+        int64_t mid_idx = -1;
+        while (start_idx < end_idx) {
+            mid_idx = (start_idx + end_idx) / 2;
+            int64_t mid_version = mid_idx + 1;
+            if (isBadVersion(mid_version)) {
+                first_bad_idx = mid_version;
+                end_idx = mid_idx;
+            } else {
+                start_idx = mid_idx + 1;
+            }
+        }
+        if (start_idx == end_idx && isBadVersion(start_idx + 1)) {
+            first_bad_idx = start_idx;
+        }
+        return first_bad_idx + 1;
+    }
+    
+    
+    int firstBadVersion_v0(int n) {
         int64_t first_one_idx = -1;
         
         int64_t start_idx = 0;
