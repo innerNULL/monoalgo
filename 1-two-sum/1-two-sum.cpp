0d0ac3345a4b3ce66b440b13726dafbe14370ab6
inull <10429190+innerNULL@users.noreply.github.com>
2022-09-25 22:03:36 +0800

    Time: 24 ms (60.98%), Space: 10.9 MB (46.28%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int32_t, int32_t> recorder = {};
+        std::vector<int32_t> result;
+        for (int32_t i = 0; i < nums.size(); ++i) {
+            const int32_t curr = nums[i];
+            const int32_t left = target - curr;
+            if (recorder.find(left) != recorder.end()) {
+                result.emplace_back(i);
+                result.emplace_back(recorder[left]);
+                return result;
+            }
+            recorder[curr] = i;
+        }
+        return result;
+    }
+
+    vector<int> twoSum_v0(vector<int>& nums, int target) {
         int current_sum;
         vector<int> output;
         
