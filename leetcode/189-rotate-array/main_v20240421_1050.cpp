31aec9d9281c602505701f6c2fb6cb8f42142ade
innerNULL <lyt697@outlook.com>
2024-04-21 10:50:51 +0800

    leetcode: 189-rotate-array

---
+++ leetcode/189-rotate-array/main_v20240421_1050.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % static_cast<int32_t>(nums.size());
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin() + k);
+        std::reverse(nums.begin() + k, nums.end());
+    }
+};
