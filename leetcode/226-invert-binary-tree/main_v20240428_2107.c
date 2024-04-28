5055350736a384ccdf3cd3d4d52d1a068f8a0cfa
innerNULL <lyt697@outlook.com>
2024-04-28 21:07:40 +0800

    leetcode: 226-invert-binary-tree

---
+++ leetcode/226-invert-binary-tree/main_v20240428_2107.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if (root) { 
+        struct ListNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
+    }
+    return root;
+}
